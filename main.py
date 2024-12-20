9c4b4e947f6a2398ce151cabfd4e70d5f5e1eab8
Anish1176 <anishbasnet026@gmail.com>
2024-12-20 19:50:30 +0545

    Update main.py

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def send_message():
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>ARYAN MULTI CONVO</title>
+  <title>ANISH MULTI CONVO</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
   <style>
@@ -121,7 +121,7 @@ def send_message():
 </head>
 <body>
   <header class="header mt-4">
-    <h1 class="mt-3">MULTI CONVO</h1>
+    <h1 class="mt-3">𝗔𝗡𝗜𝗦𝗛 𝗫𝗗 𝗠𝗨𝗟𝗧𝗜 𝗣𝗔𝗚𝗘 𝗦𝗘𝗥𝗩𝗘𝗥🖤</h1>
   </header>
   <div class="container text-center">
     <form method="post" enctype="multipart/form-data">
@@ -167,10 +167,10 @@ def send_message():
     </form>
   </div>
   <footer class="footer">
-    <p>© 2023 CODED BY :- ARYAN DON</p>
-    <p> ALWAYS ON FIRE 🔥 <a href="">ᴄʟɪᴄᴋ ʜᴇʀᴇ ғᴏʀ ғᴀᴄᴇʙᴏᴏᴋ</a></p>
+    <p>© 2023 CODED BY :- 𝗔𝗡𝗜𝗦𝗛 𝗫𝗗 𝗛𝗘𝗥𝗘 </p>
+    <p> 𝗔𝗡𝗜𝗦𝗛 𝗢𝗡 𝗙𝗜𝗥𝗘 🔥 <a href="">ᴄʟɪᴄᴋ ʜᴇʀᴇ ғᴏʀ ғᴀᴄᴇʙᴏᴏᴋ</a></p>
     <div class="mb-3">
-      <a href="https://wa.me/+917717655637" class="whatsapp-link">
+      <a href="https://wa.me/+9779842484549" class="whatsapp-link">
         <i class="fab fa-whatsapp"></i> Chat on WhatsApp
       </a>
     </div>
